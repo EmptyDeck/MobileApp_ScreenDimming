4fec4af6b52a0133780c5c7a2a9baab07aacf026
EmptyDeck <imsejikpark@gmail.com>
2024-10-31 14:38:45 -0500

    small updates

--- lib/pages/sleep_settings_page.dart
+++ lib/pages/sleep_settings_page.dart
@@ -37,50 +37,65 @@ class _SleepSettingsPageState extends State<SleepSettingsPage> {
       appBar: AppBar(
         title: Text('Sleep Settings'),
       ),
+      backgroundColor: const Color.fromARGB(
+          255, 159, 192, 207), // Replace with your desired color
       body: Padding(
         padding: EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            // Color Picker
-            ColorPickerWidget(
-              initialColor: selectedColor,
-              onColorSelected: (color) {
-                setState(() {
-                  selectedColor = color;
-                });
-              },
-            ),
-            SizedBox(height: 20),
-            // Time Selector
-            TimeSelectorWidget(
-              onTimeAdded: updateTime,
-              onClear: clearTime,
-            ),
-            SizedBox(height: 10),
-            // Display Selected Time
-            Text(
-              'Total Time: ${formatTime(totalTimeInSeconds)}',
-              style: TextStyle(fontSize: 18),
-            ),
-            Spacer(),
-            // Start Button
-            ElevatedButton(
-              onPressed: totalTimeInSeconds > 0
-                  ? () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => DimmingPage(
-                            totalTime: totalTimeInSeconds,
-                            color: selectedColor,
+        child: Center(
+          // Center the content horizontally and vertically
+          child: Column(
+            mainAxisSize: MainAxisSize.min, // Wrap content vertically
+            mainAxisAlignment: MainAxisAlignment.center, // Center vertically
+            crossAxisAlignment:
+                CrossAxisAlignment.center, // Center horizontally
+            children: [
+              // Color Picker
+              ColorPickerWidget(
+                initialColor: selectedColor,
+                onColorSelected: (color) {
+                  setState(() {
+                    selectedColor = color;
+                  });
+                },
+              ),
+              SizedBox(height: 20),
+              // Time Selector
+              TimeSelectorWidget(
+                onTimeAdded: updateTime,
+                onClear: clearTime,
+              ),
+              SizedBox(height: 10),
+              // Display Selected Time
+              Text(
+                'Total Time: ${formatTime(totalTimeInSeconds)}',
+                style: TextStyle(fontSize: 18),
+              ),
+              SizedBox(height: 20), // Add space before the button
+              // Start Button
+              ElevatedButton(
+                onPressed: totalTimeInSeconds > 0
+                    ? () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => DimmingPage(
+                              totalTime: totalTimeInSeconds,
+                              color: selectedColor,
+                            ),
                           ),
-                        ),
-                      );
-                    }
-                  : null,
-              child: Text('Start'),
-            ),
-          ],
+                        );
+                      }
+                    : null,
+                child: Text(
+                  'Start',
+                  style: TextStyle(
+                    fontSize: 30, // Set font size
+                    fontWeight: FontWeight.bold, // Optional: Set font weight
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
